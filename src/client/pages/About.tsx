6c0fe58996e451d83e79d543f1f0c0be920d412d
DoppelGinger <mr.tachibana@mail.ru>
2025-07-13 23:56:28 +0500

    test

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>Author: Васинович Степан</p>
+          <p>Author: Васинович Степан а еще тестовый текст для ПР</p>
         </div>
       </div>
     </div>
